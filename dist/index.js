63505229cc678d04706e9e48e48c71fbffb63685
Sam Faulkner <samfaulkner@Sams-MacBook-Pro.local>
2022-11-21 14:31:03 +0000

    new profile look and fixed messaging bug

--- dist/index.js
+++ dist/index.js
@@ -5,7 +5,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express = require('express');
 const index_1 = __importDefault(require("./routes/index"));
+const cors_1 = __importDefault(require("cors"));
 const app = express();
+app.use((0, cors_1.default)());
 app.use(express.json());
 app.use(index_1.default);
 app.listen(process.env.PORT || 8080, () => console.log('listenin'));
