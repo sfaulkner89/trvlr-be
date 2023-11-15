6302e2c8b218c442737a637356f27c74b1519d64
Sam Faulkner <samfaulkner@Biggie.local>
2023-11-15 16:43:27 +0000

    YOU HAVE TO BE KIDDING

--- graphql/mutations/index.ts
+++ graphql/mutations/index.ts
@@ -6,7 +6,6 @@ import { createList } from './lists/createList'
 import { createPlace } from './places/createPlace'
 import { addPlaceToList } from './lists/addPlaceToList'
 import { postMessage } from './messages/postMessage'
-// @ts-ignore:next-line
 import deleteList from './lists/deleteList'
 import { createGroup } from './messages/createGroup'
 import { putContact } from './users/putContact'
